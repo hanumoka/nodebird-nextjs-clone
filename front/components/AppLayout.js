4b43fb41d8bf44130f6fdf19efffa3942986e9c0
amagramer <amagramer@gmail.com>
2022-03-14 23:39:10 +0900

    next.js 최ì초기 셋팅

---
+++ front/components/AppLayout.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+const AppLayout = ({children}) =>{
+    return(
+        <div>
+            <Link href="/">
+                <a>노드버드</a>
+            </Link>
+            <Link href="/profile">
+                <a>프로필</a>
+            </Link>
+            <Link href="/signup">
+                <a>회원가입</a>
+            </Link>
+            <div>공통메뉴</div>
+            {children}
+        </div>
+    )
+}
+
+AppLayout.prototype = {
+    children: PropTypes.node.isRequired,
+}
+
+export default AppLayout;
