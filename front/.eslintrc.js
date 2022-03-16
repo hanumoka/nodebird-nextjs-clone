7ac192fcb0c2bba4ea3483e8fd1bc1fdada2045d
amagramer <amagramer@gmail.com>
2022-03-17 06:42:21 +0900

    1

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -33,5 +33,10 @@ module.exports = {
     'react/jsx-props-no-spreading': [1, { custom: 'ignore' }],
     'jsx-a11y/label-has-associated-control': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'object-curly-newline': 'off',
+    'linebreak-style': 'off',
+    'no-param-reassign': 'off',
   },
 };
