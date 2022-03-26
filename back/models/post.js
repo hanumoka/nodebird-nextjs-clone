86ae372a57072eaa6a19861c5cf974fa55bf8be4
amagramer <amagramer@gmail.com>
2022-03-26 11:05:53 +0900

    ㅡãmodadd models and add nodemone

--- back/models/post.js
+++ back/models/post.js
@@ -12,6 +12,14 @@ module.exports = (sequelize, DataTypes) => {
       collate: "utf8mb4_general_ci",
     }
   );
-  Post.associate = (db) => {};
+  Post.associate = (db) => {
+    db.Post.belongsTo(db.User);
+    db.Post.belongsToMany(db.Hashtag, { through: "PostHashtag" });
+    db.Post.hasMany(db.Comment);
+    db.Post.hasMany(db.Image);
+    // User <=> Like(좋아요), User와 Post 사이에 다대다 관계 테이블 <=> Post
+    db.Post.belongsToMany(db.User, { through: "Like", as: "Likers" });
+    db.Post.belongsTo(db.Post, { as: "Retweet" });
+  };
   return Post;
 };
--- back/models/user.js
+++ back/models/user.js
@@ -23,6 +23,24 @@ module.exports = (sequelize, DataTypes) => {
       collate: "utf8_general_ci", // 한글 저장
     }
   );
-  User.associate = (db) => {};
+  User.associate = (db) => {
+    db.User.hasMany(db.Post);
+    db.User.hasMany(db.Comment);
+    // User <=> Like(좋아요), User와 Post 사이에 다대다 관계 테이블 <=> Post, 안티패턴같은데...
+    db.User.belongsToMany(db.Post, { through: "Like", as: "Liked" });
+    // through: 테이블 명
+    // as : 대상 모델의 별칭
+    // foreignKey : 상대 테이블 컬럼명
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followers",
+      foreignKey: "FollowingId",
+    });
+    db.User.belongsToMany(db.User, {
+      through: "Follow",
+      as: "Followings",
+      foreignKey: "FollowerId",
+    });
+  };
   return User;
 };
