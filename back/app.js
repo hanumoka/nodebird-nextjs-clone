3a627d9f2406c29cac2e87039f03769638345b23
amagramer <amagramer@gmail.com>
2022-03-26 12:24:31 +0900

    í회원가입 ì¶연동

--- back/app.js
+++ back/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
 const db = require("./models");
 
 const app = express();
@@ -9,6 +11,9 @@ db.sequelize
     console.log("db 연결 성공");
   })
   .catch(console.error);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello api");
@@ -23,6 +28,7 @@ app.get("/posts", (req, res) => {
 });
 
 app.use("/post", postRouter);
+app.use("/user", userRouter);
 
 app.listen(3065, () => {
   console.log("node server start ... port 3065");
